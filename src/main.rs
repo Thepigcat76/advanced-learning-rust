c2e7cf170d1c29c1217a36824f21a7d17566aaf9
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-05-17 22:30:44 +0200

    friends list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let mut friends: Vec<String> = vec!["uwu".to_string(), "owo".to_string(), "#w#".to_string()];
+    loop {
+        println!("Enter a new name:");
+        let input = scan();
+        if input.is_empty() {
+            break; // Break the loop if an empty input is provided
+        }
+        friends.push(input);
+        for friend in &friends {
+            println!("{}", friend);
+        }
+    }
+}
+
+fn scan() -> String {
+    let mut input = String::new();
+    io::stdin()
+        .lock()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let trimmed_input = input.trim().to_owned();
+    return trimmed_input;
 }
